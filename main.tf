955a855fdef1a7f347904229a44019e831d52325
merc.lorraine <merc.lorraine@gmail.com>
2026-07-29 20:11:40 -0400

    Main.tf for forge pipeline

--- main.tf
+++ main.tf
@@ -2,9 +2,14 @@ provider "aws" {
   region = "us-east-1"  
 }  
   
-resource "aws_ssm_parameter" "drone_check" {  
-  name  = "/tkh/pipeline/status-merc"  
-  type  = "String"  
-  value = "Deployment Drone Successful via OIDC"  
-  overwrite = true
+resource "aws_security_group" "sabotaged_sg" {  
+  name        = "tlab7-exposed-sg"  
+  description = "A dangerously exposed security group"  
+  
+  ingress {  
+    from_port   = 22  
+    to_port     = 22  
+    protocol    = "tcp"  
+    cidr_blocks = ["0.0.0.0/0"] # SABOTAGE: SSH exposed to the world  
+  }  
 }  
